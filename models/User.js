452e9440425583b2b4b786a472ed836e1be45ab3
dev <idfkdev@gmail.com>
2025-06-13 22:07:16 +0545

    Nothing more

--- models/User.js
+++ models/User.js
@@ -1,20 +1,23 @@
-const { DataTypes } = require('sequelize');
-const sequelize = require('../database/config');
+const { DataTypes } = require("sequelize");
+const sequelize = require("../database/config");
 
-const User = sequelize.define('User',{
+const User = sequelize.define(
+  "User",
+  {
     username: {
-        type:DataTypes.STRING,
-        allowNull: false
-    }, 
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
     email: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        unique: true
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
     },
     password: {
-        type: DataTypes.STRING,
-        allowNull: false
-    }
-});
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  }
+);
 
 module.exports = User;
